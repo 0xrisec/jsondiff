46cc24559d3e935ae39682f88c30ffb39b5348c1
rootbabu <babuhacker360@gmail.com>
2025-01-15 11:01:27 +0530

    Add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
               gtag('config', 'G-LQ7QXW3VKP');
           `}
       </Script>
+      {/* <head>
+        <AdSense pId="7160351643450161"/>
+      </head> */}
+      <Script type="text/javascript">
+        
+        {
+        `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "pt6tzki4g6");
+        `
+      }
+    </Script>
       <body>{children}</body>
     </html>
   );
